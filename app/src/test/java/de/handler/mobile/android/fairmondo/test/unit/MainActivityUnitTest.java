c65df07e1db5631a371726de75b5e67462f501b9
Nino Handler <ninohandler@gmail.com>
2015-03-08 01:39:43 +0100

    updated to gradle 1.1.0

--- app/src/test/java/de/handler/mobile/android/fairmondo/test/unit/MainActivityUnitTest.java
+++ app/src/test/java/de/handler/mobile/android/fairmondo/test/unit/MainActivityUnitTest.java
@@ -1,7 +1,10 @@
 package de.handler.mobile.android.fairmondo.test.unit;
 
 import android.test.AndroidTestCase;
-
+/**
+ * You have to activate UnitTesting support in Settings... --> Gradle --> experimental.
+ * And to the left choose BuildVariants and change to Unit Tests
+ */
 public class MainActivityUnitTest extends AndroidTestCase {
 
     public void testOnCategoriesResponse() throws Exception {
