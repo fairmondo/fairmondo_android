b53873be01dbcc14ef03be7c6734ac81cdbebea2
Nino Handler <ninohandler@gmail.com>
2015-03-15 21:12:52 +0100

    commented the CustomNetworkImageView.java

--- app/src/main/java/de/handler/mobile/android/fairmondo/ui/views/CustomNetworkImageView.java
+++ app/src/main/java/de/handler/mobile/android/fairmondo/ui/views/CustomNetworkImageView.java
@@ -12,17 +12,40 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 
 /**
- * Workaround as setting Bitmap or ImageDrawable with NetworkImageView did not work out of the box
+ * Custom implementation of Volley's NetworkImageView which enriches the class with a fade
+ * when the image has been retrieved and corrects a bug which makes it impossible to set
+ * bitmaps manually.
  */
 public class CustomNetworkImageView extends NetworkImageView {
     private static final int FADE_IN_TIME_MS = 350;
-
     private Bitmap  mLocalBitmap;
-    private BitmapDrawable mLocalDrawable;
-
     private boolean mShowLocalBitmap;
-    private boolean mShowLocalDrawable;
 
+    /**
+     * First of three standard constructors for custom views.
+     */
+    public CustomNetworkImageView(Context context) {
+        this(context, null);
+    }
+
+    /**
+     * Second of three standard constructors for custom views.
+     */
+    public CustomNetworkImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    /**
+     * Third of three standard constructors for custom views.
+     */
+    public CustomNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * Set a bitmap manually.
+     * @param bitmap the image which should be displayed
+     */
     public void setLocalImageBitmap(Bitmap bitmap) {
         if (bitmap != null) {
             mShowLocalBitmap = true;
@@ -31,34 +54,21 @@ public class CustomNetworkImageView extends NetworkImageView {
         requestLayout();
     }
 
-    public void setLocalImageDrawable(BitmapDrawable drawable) {
-        if (drawable != null) {
-            mShowLocalDrawable = true;
-        }
-        this.mLocalDrawable = drawable;
-        requestLayout();
-    }
-
+    /**
+     * set the image url from where the image should be retrieved.
+     * @param url the url from which the image should be retrieved
+     * @param imageLoader the image loader volley uses to get the images from the url
+     */
     @Override
     public void setImageUrl(String url, ImageLoader imageLoader) {
         mShowLocalBitmap = false;
-        mShowLocalDrawable = false;
         super.setImageUrl(url, imageLoader);
     }
 
-    public CustomNetworkImageView(Context context) {
-        this(context, null);
-    }
-
-    public CustomNetworkImageView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public CustomNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-
+    /**
+     * Add a fade to the standard setImageBitmap method.
+     * @param bm the image which has just been retrieved from volley
+     */
     @Override
     public void setImageBitmap(Bitmap bm) {
         TransitionDrawable td = new TransitionDrawable(new Drawable[]{
@@ -70,15 +80,14 @@ public class CustomNetworkImageView extends NetworkImageView {
         td.startTransition(FADE_IN_TIME_MS);
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-
         super.onLayout(changed, left, top, right, bottom);
         if (mShowLocalBitmap) {
             setImageBitmap(mLocalBitmap);
-        } else if (mShowLocalDrawable) {
-            setImageDrawable(mLocalDrawable);
         }
     }
 }
