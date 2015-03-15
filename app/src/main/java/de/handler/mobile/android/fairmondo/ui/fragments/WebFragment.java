1f28c6ee00a99978e5b96b2af20937063dbe42e9
Nino Handler <ninohandler@gmail.com>
2015-03-15 21:31:45 +0100

    commented WebFragment.java

--- app/src/main/java/de/handler/mobile/android/fairmondo/ui/fragments/WebFragment.java
+++ app/src/main/java/de/handler/mobile/android/fairmondo/ui/fragments/WebFragment.java
@@ -1,5 +1,6 @@
 package de.handler.mobile.android.fairmondo.ui.fragments;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -20,7 +21,7 @@ import org.androidannotations.annotations.ViewById;
 import de.handler.mobile.android.fairmondo.R;
 
 /**
- * Displays Web Content
+ * Displays Web Content.
  */
 @EFragment(R.layout.fragment_web)
 public class WebFragment extends Fragment {
@@ -28,7 +29,7 @@ public class WebFragment extends Fragment {
     public static final String HTTP_CONTENT = "http_content";
     public static final String COOKIE = "cart_cookie";
 
-    String mCookie = "";
+    private String mCookie = "";
 
     @ViewById(R.id.fragment_web_webview)
     WebView webView;
@@ -36,31 +37,36 @@ public class WebFragment extends Fragment {
     @ViewById(R.id.fragment_web_progress_container)
     RelativeLayout progressContainer;
 
-    //TODO after redirect 500 from fairmondo page --> cookie not set correctly?
     @AfterViews
     public void init() {
-        String http = getArguments().getString(HTTP_CONTENT);
+        // Get the bundle arguments
+        String html = getArguments().getString(HTTP_CONTENT);
         String uri = getArguments().getString(URI);
         mCookie = getArguments().getString(COOKIE);
 
-        if (http != null) {
-            webView.loadData(http, "text/html; charset=UTF-8", null);
-        } else if (uri != null && !uri.equals("")) {
+        // if the html string is null a uri shall be displayed
+        if (html == null) {
+            if (uri == null || uri.equals("")) {
+                return;
+            }
             progressContainer.setVisibility(View.VISIBLE);
-
-            if (mCookie != null) {
-                this.setCookie(webView, mCookie, uri);
-            } else {
+            if (mCookie == null) {
                 webView.setWebViewClient(new RedirectWebViewClient());
+            } else {
+                this.setCookie(webView, mCookie, uri);
             }
             WebSettings webSettings = webView.getSettings();
             webSettings.setJavaScriptEnabled(true);
             webView.loadUrl(uri);
+        } else {
+            webView.loadData(html, "text/html; charset=UTF-8", null);
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     void setCookie(WebView webView, String cookie, String uri) {
         webView.setWebViewClient(new RedirectWebViewClient(false));
+        // Since LOLLIPOP the cookie manager has not to be explicitly retrieved any more.
         if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             CookieSyncManager.createInstance(getActivity());
         }
@@ -72,13 +78,25 @@ public class WebFragment extends Fragment {
         }
     }
 
+    /**
+     * Web View Client which
+     * 1. Displays a ProgressBar until the content is completely shown and
+     * 2. Prompts the user to use its own browser to be able to navigate to further links.
+     * Workaround for dividing the app from the internet page as some content as the cart
+     * up to now only works on the internet page.
+     */
     private class RedirectWebViewClient extends WebViewClient {
         boolean handleKeyEvent = true;
+
         RedirectWebViewClient (boolean handleKeyEvent) {
             this.handleKeyEvent = handleKeyEvent;
         }
+
         RedirectWebViewClient() {}
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             super.shouldOverrideUrlLoading(view, url);
@@ -96,6 +114,9 @@ public class WebFragment extends Fragment {
             return handleKeyEvent;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
