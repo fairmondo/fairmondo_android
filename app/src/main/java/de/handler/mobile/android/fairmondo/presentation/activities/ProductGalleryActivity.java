92105f2c5d40247e17f9011215469c661f0d6192
Morton Cornelius <s0534592@htw-berlin.de>
2015-07-27 23:14:08 +0200

    corrected sharing url and some minor layout issues

--- app/src/main/java/de/handler/mobile/android/fairmondo/presentation/activities/ProductGalleryActivity.java
+++ app/src/main/java/de/handler/mobile/android/fairmondo/presentation/activities/ProductGalleryActivity.java
@@ -87,7 +87,7 @@ public class ProductGalleryActivity extends AbstractActivity {
     // Build the string which is used to share a fairmondo
     // product recommendation with other android apps
     private String buildSharingText(final Product product) {
-        return getString(R.string.share_text) + product.getHtmlUrl();
+        return getString(R.string.share_text) + getString(R.string.base_url) + product.getSlug();
     }
 
     // Call to update the share intent
